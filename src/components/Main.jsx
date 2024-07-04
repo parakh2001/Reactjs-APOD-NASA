5860f5e8db7e86ee564fe853f953b2d956f7ffcb
parakh2001 <parakh.gupta2571@gmail.com>
2024-07-04 18:34:35 +0530

    fix: slow loading of image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 export const Main = (props) => {
-  const {data} = props;
+  const { data } = props;
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <div>
-      <img src={data.hdurl} alt={data.title} className='bgImage'/>
+      <img
+        src={data.url} // Low-resolution image URL
+        alt={data.title}
+        className='bgImage low-res'
+        style={{ display: imageLoaded ? 'none' : 'block' }}
+      />
+      <img
+        src={data.hdurl} // High-resolution image URL
+        alt={data.title}
+        className='bgImage high-res'
+        onLoad={() => setImageLoaded(true)}
+        style={{ display: imageLoaded ? 'block' : 'none' }}
+      />
     </div>
   );
-}
+};
